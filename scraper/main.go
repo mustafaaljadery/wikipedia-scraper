65023bb1c912209465530de5ebf87ad9678dd256
Mustafa Aljadery <mustafaaljadery1@gmail.com>
2023-10-15 17:37:27 -0700

    add main

--- scraper/main.go
+++ scraper/main.go
@@ -22,12 +22,16 @@ func Scrape(){
 
 func Tokenize(){
 	docs := tokenizer.GetDocs()
-	doc := tokenizer.GetDoc(docs[0])
 
-	fmt.Println(tokenizer.Encode(doc))
+	for _, doc_path := range docs {
+		doc := tokenizer.GetDoc(doc_path)
+		tokenizer.Encode(doc)
+    }
+
+	fmt.Println("Tokenized", len(docs), "docs.")
 }
 
 func main(){
-	//Scrape()
-	Tokenize()
+	Scrape()
+	//Tokenize()
 }
